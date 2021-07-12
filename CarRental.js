3517c7a8e4548006a5526342b17e244f5bc692ae
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-07-12 20:00:49 +0300

    car rental completed

--- CarRental.js
+++ CarRental.js
@@ -1,11 +1,19 @@
 class CarRental {
     constructor() {
         this.list = []
-
+        this.brokenCar = [];
     }
     intro() {
-        let count = this.list.length
+        let count = 0
+        for (let i = 0; i < this.list.length; i++) {
+            const object = this.list[i];
+            if (object.working === true) {
+                count++
+            }
+
+        }
         console.log(`Hi, we are CityBird and we have ${count} cars available.`);
+
     }
 
     addCar(model, color, price) {
@@ -13,40 +21,40 @@ class CarRental {
         this.list.push({
             'model': model,
             'color': color,
-            'price': price
+            'price': price,
+            working: true
         })
         // console.log(this.list);
     }
 
     carPark() {
+        let position = 0
         for (let i = 0; i < this.list.length; i++) {
             const object = this.list[i];
-            console.log(
-                `${[i + 1]}. ${object.model} (${object.color}) is for ${object.price}EUR/day.`
-            );
+            if (object.working === true) {
+                position++
+                console.log(
+                    `${[position]}. ${object.model} (${object.color}) is for ${object.price}EUR/day.`
+                )
+            }
         }
     }
     updateCarPrice(index, newPrice) {
         this.list[index].price = newPrice
-        console.log(this.list);
+        // console.log(this.list);
     }
 
-    carPark() {
-        for (let i = 0; i < this.list.length; i++) {
-            const object = this.list[i];
-            console.log(
-                `${[i + 1]}. ${object.model} (${object.color}) is for ${object.price}EUR/day.`
-            );
-        }
-    }
 
     carAccident(index) {
+        this.list[index].working = false;
+    }
+
+
+    carRepair(index) {
+        this.list[index].working = true;
 
     }
-    carRepair(index) {
-        for (let i = 0; i < this.list.length; i++) {
-        }
-    }
+
 
 
     removeCar(index) {
@@ -56,8 +64,9 @@ class CarRental {
             if (i !== index) {
                 newList.push(element)
             }
-            this.list = newList;
-        }
-    }
 
+        }
+        this.list = newList;
+    }
+}
 module.exports = CarRental;
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const CarRental = require('./CarRental');
 
 const cityBird = new CarRental('CityBird');
 
-cityBird.intro();
-// Hi, we are CityBird and we have 0 cars available.
+// cityBird.intro();
+// // Hi, we are CityBird and we have 0 cars available.
 
 cityBird.addCar('Toyota Yaris', 'black', 25);
 cityBird.addCar('Volvo S40', 'red', 20);
@@ -33,23 +33,23 @@ cityBird.carPark();
 // // 1. Toyota Yaris (black) is for 22EUR/day.
 // // 2. Tesla 3 (white) is for 40EUR/day.
 
-cityBird.carRepair(1);
+// cityBird.carRepair(1);
 
 // cityBird.intro();
-// // Hi, we are CityBird and we have 3 cars available.
+// // // Hi, we are CityBird and we have 3 cars available.
 
 // cityBird.carPark();
-// // 1. Toyota Yaris (black) is for 22EUR/day.
-// // 2. Volvo S40 (red) is for 20EUR/day.
-// // 3. Tesla 3 (white) is for 40EUR/day.
+// // // 1. Toyota Yaris (black) is for 22EUR/day.
+// // // 2. Volvo S40 (red) is for 20EUR/day.
+// // // 3. Tesla 3 (white) is for 40EUR/day.
 
-cityBird.removeCar(0);
+// cityBird.removeCar(0);
 
-cityBird.updateCarPrice(1, 35);
+// cityBird.updateCarPrice(1, 35);
 
-cityBird.intro();
-// // Hi, we are CityBird and we have 2 cars available.
+// cityBird.intro();
+// // // Hi, we are CityBird and we have 2 cars available.
 
-cityBird.carPark();
+// cityBird.carPark();
 // // 1. Volvo S40 (red) is for 20EUR/day.
 // // 2. Tesla 3 (white) is for 35EUR/day.
